3a0693314356bb3edc7b9122ad63f473757b3dc2
abstha <108931616+abstha@users.noreply.github.com>
2023-11-24 10:08:33 +0545

    removed fixture arrays

--- cypress.config.js
+++ cypress.config.js
@@ -10,6 +10,7 @@ module.exports = defineConfig({
     reporter: "mochawesome",
     reporterOptions: {
       reportDir: "cypress/reports/mochawesome",
+      reportFilename: "[name]-report",
       overwrite: true,
       html: true,
       json: true,

a2207f5b32f8db18e5ac458dffc749882ef15767
abstha <108931616+abstha@users.noreply.github.com>
2023-12-05 12:19:53 +0545

    added more verification for checking data usage

--- cypress/e2e/MainSite.cy.js
+++ cypress/e2e/MainSite.cy.js
@@ -1,15 +1,19 @@
 const { describe } = require("mocha");
+const dat = require("../fixtures/mock.json");
 
-describe("Tests for Different Fixtures", () => {
-  it("test check", () => {
-    cy.fixture("TestData.json").then((data) => {
+
+function pt(fixtures, data) {
+  describe("Tests for Different Fixtures", () => {
+    it("test check", () => {
       cy.visit("https://www.ncell.axiata.com/en/auth/login");
 
       cy.get('input[placeholder="Enter mobile number"]').type(
-        data[0].phoneNumber
+        fixtures.phoneNumber
       );
 
-      cy.get('input[placeholder="Password"]').type(data[0].password);
+      cy.get('input[placeholder="Password"]').type(fixtures.password);
+
+      cy.get("input[id='checkboxSignINTerms']").click({ force: true });
 
       cy.get("input.custom-control-input").check({
         force: true,
@@ -19,18 +23,41 @@ describe("Tests for Different Fixtures", () => {
 
       cy.wait(7000);
 
-      cy.get(".main_details").children().should("have.length", 2);
+      console.log(fixtures.dashboardName.length);
 
-      cy.get(".list_title").contains("Elearning Data").should("exist");
+      cy.get(".main_details").children().should("have.length.gte", 2);
+      for (let i = 0; i < fixtures.dashboardName.length; i++) {
+        cy.get(".list_title")
+          .contains(fixtures.dashboardName[i])
+          .should("exist");
+      }
 
-      cy.get(".list_title")
-        .contains("Elearning Data")
-        .parent() 
-        .within(() => {
-          cy.get(".list_info span:last-child").should("contain", "25.00 GB");
-        });
+      for (let i = 0; i < fixtures.dashboardName.length; i++) {
+        cy.get(".dashboard_data_list")
+          .contains(fixtures.dashboardName[i])
+          .parent()
+          .within(() => {
+            cy.get(".list_cs span:last-child").then(($el) => {
+              const val = $el.text();
+              const num = parseFloat(val.match(/\d+\.\d+/)[0]);
+              const inval = Math.floor(num);
+              let bval = Math.floor(inval); // can be deleted
+              console.log(bval) //cannot be returned
+              let packVal = fixtures.packResource[i];
+              
+              const convNum = parseFloat(packVal.match(/\d+\.\d+/)[0]);
+              const convVal = Math.floor(convNum);
 
-      cy.pause();
+              expect(inval).to.be.equal(convVal);
+            });
+          });
+      }
     });
+  });
+}
+
+dat.forEach((fixture, index) => {
+  describe(`Pack Name: ${fixture.packName}`, () => {
+    pt(fixture, index);
   });
 });
--- cypress/e2e/SanityTesting.cy.js
+++ cypress/e2e/SanityTesting.cy.js
@@ -13,116 +13,60 @@ function Login(number, password) {
 }
 
 function performTests(fixtureData, index) {
-  describe("low balance testing", () => {
-    it("checks if the balance is enough to buy the data pack", () => {
-      cy.exec(
-        'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" balance',
-        { log: true }
-      ).then(({ stdout, stderr }) => {
-        if (stdout) {
-          cy.log(`Standard output:\n${stdout}`);
-        }
-        if (stderr) {
-          cy.log(`Error output:\n${stderr}`);
-        }
-      });
-      cy.readFile(
-        "C:/Users/abhinav/Desktop/automation tests/cypress test/resourceDetails/OTP.json",
-        "utf8"
-      ).then((data) => {
-        const parsedBalance = parseFloat(data.balance);
-        const packValueString = fixtureData.pack;
-        const match = packValueString.match(/\d+/);
-        const packValue = parseInt(match[0]);
-
-        console.log(parsedBalance, "parsedbalance");
-        console.log(match[0], "packvalue");
-
-        if (parsedBalance < match[0]) {
-          cy.log("Balance is insufficient to buy the data pack");
-          expect(parsedBalance).to.be.lessThan(packValue);
-        } else if (parsedBalance >= match[0]) {
-          cy.log("Balance is sufficient to buy the data pack");
-          expect(parsedBalance).to.be.gte(packValue);
-        } else {
-          throw new Error("Invalid balance value");
-        }
-      });
-    });
-
-    it("buys the Rs 265 voice pack", () => {
-      cy.visit(
-        "https://www.ncell.axiata.com/en/individual/data-and-voice?packageCategories=data-packs"
-      );
-
-      cy.contains("Load More").click();
-      cy.wait(2000);
-      // cy.contains(testData.packName).click();
-
-      cy.contains(fixtureData.pack).click();
-      cy.contains("Buy Now").click();
-      cy.get('[name="phoneNumber"]').type(fixtureData.phoneNumber);
-      cy.contains("Continue").click();
-
-      cy.exec(
-        'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" otp BuyOTP',
-        { log: true }
-      ).then(({ stdout, stderr }) => {
-        if (stdout) {
-          cy.log(`Standard output:\n${stdout}`);
-        }
-        if (stderr) {
-          cy.log(`Error output:\n${stderr}`);
-        }
-
+    describe("low balance testing", () => {
+      it("checks if the balance is enough to buy the data pack", () => {
+        cy.exec(
+          'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" balance',
+          { log: true }
+        ).then(({ stdout, stderr }) => {
+          if (stdout) {
+            cy.log(`Standard output:\n${stdout}`);
+          }
+          
+          if (stderr) {
+            cy.log(`Error output:\n${stderr}`);
+          }
+        });
         cy.readFile(
           "C:/Users/abhinav/Desktop/automation tests/cypress test/resourceDetails/OTP.json",
           "utf8"
-        ).then((otpObject) => {
-          const otp = otpObject.BuyOTP;
-          cy.log(`Read OTP value: ${otp}`);
+        ).then((data) => {
+          const parsedBalance = parseFloat(data.balance);
+          const packValueString = fixtureData.pack;
+          const match = packValueString.match(/\d+/);
+          const packValue = parseInt(match[0]);
 
-          for (let i = 0; i < otp.length; i++) {
-            cy.get(`input[aria-label*="Character ${i + 1}"]`).type(
-              otp.charAt(i)
-            );
+          console.log(parsedBalance, "parsedbalance");
+          console.log(match[0], "packvalue");
+
+          if (parsedBalance < match[0]) {
+            cy.log("Balance is insufficient to buy the data pack");
+            expect(parsedBalance).to.be.lessThan(packValue);
+          } else if (parsedBalance >= match[0]) {
+            cy.log("Balance is sufficient to buy the data pack");
+            expect(parsedBalance).to.be.gte(packValue);
+          } else {
+            throw new Error("Invalid balance value");
           }
         });
+      });
+
+      it("buys the Rs 265 voice pack", () => {
+        cy.visit(
+          "https://www.ncell.axiata.com/en/individual/data-and-voice?packageCategories=data-packs"
+        );
+
+        cy.contains("Load More").click();
+        cy.wait(2000);
+        // cy.contains(testData.packName).click();
+
+        cy.contains(fixtureData.pack).click();
+        cy.contains("Buy Now").click();
+        cy.get('[name="phoneNumber"]').type(fixtureData.phoneNumber);
         cy.contains("Continue").click();
-        cy.contains("Insufficient Balance").should("be.visible");
-      });
-    });
-  });
 
-  describe("adequate balance testing", () => {
-    it("checks if the balance is enough to buy the data pack", () => {
-      cy.readFile(
-        "C:/Users/abhinav/Desktop/automation tests/cypress test/resourceDetails/OTP.json",
-        "utf8"
-      ).then((data) => {
-        const parsedBalance = parseFloat(data.balance);
-        const packValueString = fixtureData.pack;
-        const match = packValueString.match(/\d+/);
-        const packValue = parseInt(match[0]);
-
-        console.log(parsedBalance, "parsedbalance");
-        console.log(match[0], "packvalue");
-
-        if (parsedBalance < match[0]) {
-          cy.log("Balance is insufficient to buy the data pack");
-          expect(parsedBalance).to.be.lessThan(packValue);
-        } else if (parsedBalance >= match[0]) {
-          cy.log("Balance is sufficient to buy the data pack");
-        } else {
-          throw new Error("Invalid balance value");
-        }
-      });
-    });
-
-    it("buys the Rs 265 voice pack", () => {
-      if (index == 0) {
         cy.exec(
-          'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" buyDataPack',
+          'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" otp BuyOTP',
           { log: true }
         ).then(({ stdout, stderr }) => {
           if (stdout) {
@@ -131,39 +75,96 @@ function performTests(fixtureData, index) {
           if (stderr) {
             cy.log(`Error output:\n${stderr}`);
           }
+
+          cy.readFile(
+            "C:/Users/abhinav/Desktop/automation tests/cypress test/resourceDetails/OTP.json",
+            "utf8"
+          ).then((otpObject) => {
+            const otp = otpObject.BuyOTP;
+            cy.log(`Read OTP value: ${otp}`);
+
+            for (let i = 0; i < otp.length; i++) {
+              cy.get(`input[aria-label*="Character ${i + 1}"]`).type(
+                otp.charAt(i)
+              );
+            }
+          });
+          cy.contains("Continue").click();
+          cy.contains("Insufficient Balance").should("be.visible");
         });
-      } else {
-        cy.exec(
-          'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" buyDataPack2'
-        );
-      }
+      });
     });
 
-    it("checks if the product has been activated or not", () => {
-      cy.visit("https://www.ncell.axiata.com/en/auth/login");
-      Login(fixtureData.phoneNumber, fixtureData.password);
-      cy.wait(10000);
+    describe("adequate balance testing", () => {
+      it("checks if the balance is enough to buy the data pack", () => {
+        cy.readFile(
+          "C:/Users/abhinav/Desktop/automation tests/cypress test/resourceDetails/OTP.json",
+          "utf8"
+        ).then((data) => {
+          const parsedBalance = parseFloat(data.balance);
+          const packValueString = fixtureData.pack;
+          const match = packValueString.match(/\d+/);
+          const packValue = parseInt(match[0]);
 
-      cy.contains(fixtureData.packName);
+          console.log(parsedBalance, "parsedbalance");
+          console.log(match[0], "packvalue");
 
-      cy.contains("button.btn_secondary.btm__btn", "Details").click();
-      cy.contains(fixtureData.packResource);
+          if (parsedBalance < match[0]) {
+            cy.log("Balance is insufficient to buy the data pack");
+            expect(parsedBalance).to.be.lessThan(packValue);
+          } else if (parsedBalance >= match[0]) {
+            cy.log("Balance is sufficient to buy the data pack");
+          } else {
+            throw new Error("Invalid balance value");
+          }
+        });
+      });
 
-      cy.get("p.list_title")
-        .contains(fixtureData.dashboardName)
-        .should("exist");
-
-      cy.get(".list_title")
-        .contains(fixtureData.dashboardName)
-        .parent()
-        .within(() => {
-          cy.get(".list_info span:last-child").should(
-            "contain",
-            fixtureData.packResource
+      it("buys the Rs 265 voice pack", () => {
+        if (index == 0) {
+          cy.exec(
+            'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" buyDataPack',
+            { log: true }
+          ).then(({ stdout, stderr }) => {
+            if (stdout) {
+              cy.log(`Standard output:\n${stdout}`);
+            }
+            if (stderr) {
+              cy.log(`Error output:\n${stderr}`);
+            }
+          });
+        } else {
+          cy.exec(
+            'node "C:/Users/abhinav/Desktop/automation tests/Mobile testing/Samsung.js" buyDataPack2'
           );
-        });
+        }
+      });
+
+      it("checks if the product has been activated or not", () => {
+        cy.visit("https://www.ncell.axiata.com/en/auth/login");
+        Login(fixtureData.phoneNumber, fixtureData.password);
+        cy.wait(10000);
+
+        cy.contains(fixtureData.packName);
+
+        cy.contains("button.btn_secondary.btm__btn", "Details").click();
+        cy.contains(fixtureData.packResource);
+
+        cy.get("p.list_title")
+          .contains(fixtureData.dashboardName)
+          .should("exist");
+
+        cy.get(".list_title")
+          .contains(fixtureData.dashboardName)
+          .parent()
+          .within(() => {
+            cy.get(".list_info span:last-child").should(
+              "contain",
+              fixtureData.packResource
+            );
+          });
+      });
     });
-  });
 
   describe("Resource usage testing", () => {
     it("Uses a app for 1 minute", () => {
@@ -177,13 +178,27 @@ function performTests(fixtureData, index) {
       Login(fixtureData.phoneNumber, fixtureData.password);
 
       cy.wait(10000);
-      cy.get(".list_title")
+
+      //added new, might be changed to check for subaccount resource.
+      cy.get(".dashboard_data_list")
         .contains(fixtureData.dashboardName)
         .parent()
         .within(() => {
-          cy.get(".list_info span:first-child").should("contain", "25.00 GB");
+          cy.get(".list_cs span:last-child").then(($el) => {
+            const val = $el.text();
+            const num = parseFloat(val.match(/\d+\.\d+/)[0]);
+            const inval = Math.floor(num);
+            let packVal = fixtureData.packResource;
+            const convNum = parseFloat(packVal.match(/\d+\.\d+/)[0]);
+            const bval = Math.floor(convNum);
+
+            expect(inval).to.be.gte(bval);
+          });
         });
 
+      cy.get(".list_title").each(($element) => {
+        cy.log($element.text());
+      });
       //this checks the first span value is less than second. still in progress
       cy.get(".list_info span:first-child").then(($firstSpan) => {
         const firstValue = parseFloat($firstSpan.text());
@@ -191,8 +206,12 @@ function performTests(fixtureData, index) {
         cy.get(".list_info span:last-child").then(($secondSpan) => {
           const secondValue = parseFloat($secondSpan.text());
 
-          expect(firstValue).to.be.lessThan(secondValue);
-        }); 
+          expect(firstValue).to.be.lte(secondValue);
+        });
+      });
+
+      cy.get(".list_info > .list_cs > span").each(($element) => {
+        cy.contains($element.text());
       });
     });
   });
@@ -203,6 +222,12 @@ function performTests(fixtureData, index) {
       cy.log(fixtureData.packResource);
       cy.log(fixtureData.balance);
       cy.log(fixtureData.dashboardName);
+    });
+
+    it("checks if multiple activation resource is added or not", () => {
+      Login(fixtureData.phoneNumber, fixtureData.password);
+
+      cy.get(".dashboard_data_list").contains(fixtureData.dashboardName);
     });
   });
 }
